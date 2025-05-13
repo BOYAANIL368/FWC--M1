ddc3c8f8cfed4826f04694114de413e9b0032767
BOYAANIL368 <boyaanilkumar2003@gmail.com>
2025-05-13 12:23:17 +0530

    Add files via upload

---
+++ avr-gcc/avr-gcc.c
@@ -0,0 +1,32 @@
+// Define Arduino pins connected to segments a-g
+const int segA = 2;
+const int segB = 3;
+const int segC = 4;
+const int segD = 5;
+const int segE = 6;
+const int segF = 7;
+const int segG = 8;
+
+void setup() {
+  // Set all segment pins as OUTPUT
+  pinMode(segA, OUTPUT);
+  pinMode(segB, OUTPUT);
+  pinMode(segC, OUTPUT);
+  pinMode(segD, OUTPUT);
+  pinMode(segE, OUTPUT);
+  pinMode(segF, OUTPUT);
+  pinMode(segG, OUTPUT);
+
+  // Display '1' → segments B and C ON
+  digitalWrite(segA, LOW);
+  digitalWrite(segB, HIGH);
+  digitalWrite(segC, HIGH);
+  digitalWrite(segD, LOW);
+  digitalWrite(segE, LOW);
+  digitalWrite(segF, LOW);
+  digitalWrite(segG, LOW);
+}
+
+void loop() {
+  // No logic needed – always show '1'
+}
